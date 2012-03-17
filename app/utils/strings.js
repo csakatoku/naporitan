56157086fe8414080ccbd0331679b77acdaf2561
Chihiro SAKATOKU <csakatoku@gmail.com>
2012-03-17 15:18:34 +0000

    Transalation function _tr now takes arguments.

--- app/utils/strings.js
+++ app/utils/strings.js
@@ -13,12 +13,44 @@
         return result;
     };
 
+    var sprintfImpl = _.memoize(function(format) {
+        var r = /(%(?:\([^)]+\))?[dfs%])/;
+        var tokens = format.split(r);
+
+        return function(params) {
+            var buf = [];
+            var idx = 0;
+            for (var i = 0; i < tokens.length; i++) {
+                var token = tokens[i];
+                if (token === '%s' || token === '%d' || token == '%f') {
+                    buf.push(params[idx]);
+                    idx += 1;
+                } else if (token === '%%') {
+                    buf.push('%');
+                } else if (token.charAt(0) === '%') {
+                    var key = token.substr(2, token.length - 4);
+                    buf.push(params[key]);
+                } else {
+                    if (token.length) {
+                        buf.push(token);
+                    }
+                }
+            }
+            return buf.join('');
+        };
+    });
+
     _.extend(app.utils, {
         snakeToCamel: _.memoize(function(str) {
             var pascal = snakeToPascal(str);
             return pascal.charAt(0).toLowerCase() + pascal.substr(1);
         }),
 
-        snakeToPascal: _.memoize(snakeToPascal)
+        snakeToPascal: _.memoize(snakeToPascal),
+
+        sprintf: function(format, params) {
+            var impl = sprintfImpl(format);
+            return impl(params);
+        }
     });
 }(App));
--- app/utils/translations.js
+++ app/utils/translations.js
@@ -1,7 +1,20 @@
 (function(app) {
+    var sprintf = app.utils.sprintf;
+
     _.extend(app.utils, {
-        '_tr': function(key, params) {
-            return I18N[key] || key;
+        '_tr': function(key) {
+            var first;
+            var msgstr = I18N[key] || key;
+            if (arguments.length < 2) {
+                return msgstr;
+            }
+
+            first = arguments[1];
+            if (_.isObject(first) || _.isArray(first)) {
+                return sprintf(msgstr, first);
+            } else {
+                return sprintf(msgstr, Array.prototype.slice.call(arguments, 1));
+            }
         }
     });
 }(App));
