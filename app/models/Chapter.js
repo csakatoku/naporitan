30eb9f18b0a318b43e7a8f94b4bbd9e7d806ad2f
Chihiro SAKATOKU <csakatoku@gmail.com>
2012-03-17 16:23:02 +0000

    Make game proto-database I18N-able.

--- app/models/Chapter.js
+++ app/models/Chapter.js
@@ -1,6 +1,14 @@
 (function(app, undef) {
     "use strict";
 
+    var gettext = app.utils._tr;
+
     app.models.Chapter = Backbone.Model.extend({
+        initialize: function(args) {
+            var id = args.id;
+            this.set({
+                name: gettext('chapter_name_' + id)
+            });
+        }
     });
 }(App));
--- app/models/Mission.js
+++ app/models/Mission.js
@@ -1,6 +1,15 @@
 (function(app, undef) {
     "use strict";
 
+    var gettext = app.utils._tr;
+
     app.models.Mission = Backbone.Model.extend({
+        initialize: function(args) {
+            var id = args.id;
+            this.set({
+                name: gettext('mission_name_' + id),
+                description: gettext('mission_description_' + id)
+            });
+        }
     });
 }(App));
