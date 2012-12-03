01ed4e526ec097db6749b6f2b91288d577fb817d
Chihiro SAKATOKU <csakatoku@zynga.com>
2012-12-03 11:44:56 +0900

    Remove Django project

--- server/naporitan/naporitan/bootstrap/views.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-def default(req):
-    ctxt = RequestContext(req, {})
-    return render_to_response('bootstrap/index.html', ctxt)
